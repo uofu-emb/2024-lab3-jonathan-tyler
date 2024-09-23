616de29e852ac3c283133a9099d5919e107f4e2d
Jonathan Warner <warnerjon12@gmail.com>
2024-09-23 12:59:08 -0600

    Protected access to shared counter with semaphore

--- src/threads.c
+++ src/threads.c
@@ -19,19 +19,31 @@ int on;
 
 void side_thread(void *params)
 {
+    int cval;
 	while (1) {
         vTaskDelay(100);
-        counter += counter + 1;
-		printf("hello world from %s! Count %d\n", "thread", counter);
+        if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE) {
+            counter += counter + 1;
+            cval = counter;
+            xSemaphoreGive(semaphore);
+
+            printf("hello world from %s! Count %d\n", "thread", cval);
+        }
 	}
 }
 
 void main_thread(void *params)
 {
+    int cval;
 	while (1) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
         vTaskDelay(100);
-		printf("hello world from %s! Count %d\n", "main", counter++);
+        if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE) {
+            cval = counter++;
+            xSemaphoreGive(semaphore);
+
+            printf("hello world from %s! Count %d\n", "main", cval);
+        }
         on = !on;
 	}
 }
