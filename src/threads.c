5412b400a6d87a9b8786fdf8c57478874a5ceb8b
Jonathan Warner <warnerjon12@gmail.com>
2024-09-26 09:27:48 -0600

    Combined update_print functions into one function
     - Fixes a bug with side thread counter update
     - Counter read now happens consistently before counter increase

--- src/threads.c
+++ src/threads.c
@@ -23,7 +23,7 @@ void side_thread(void *params)
 {
 	while (1) {
         vTaskDelay(100);
-        thread_update_print(&counter, semaphore);
+        counter_update_print("thread", &counter, semaphore, NULL);
 	}
 }
 
@@ -33,7 +33,7 @@ void main_thread(void *params)
 	while (1) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
         vTaskDelay(100);
-        main_update_print(&counter, semaphore);
+        counter_update_print("main", &counter, semaphore, NULL);
         on = !on;
 	}
 }
--- src/util.c
+++ src/util.c
@@ -6,27 +6,14 @@
 #include <pico/multicore.h>
 #include <pico/cyw43_arch.h>
 
-bool thread_update_print(int* counter, SemaphoreHandle_t semaphore) {
+int counter_update_print(char* name, int* counter, SemaphoreHandle_t semaphore, int* counter_out) {
     int cval;
-    bool gotsema = (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE);
-    if (gotsema) {
-        *counter += *counter + 1;
-        cval = *counter;
-        xSemaphoreGive(semaphore);
-
-        printf("hello world from % 6s! Count % d\n", "thread", cval);
-    }
-    return gotsema;
-}
-
-bool main_update_print(int* counter, SemaphoreHandle_t semaphore) {
-    int cval;
-    bool gotsema = (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE);
-    if (gotsema) {
+    if (xSemaphoreTake(semaphore, portMAX_DELAY) == pdTRUE) {
         cval = (*counter)++;
         xSemaphoreGive(semaphore);
 
-        printf("hello world from % 6s! Count % d\n", "main", cval);
+        if (counter_out) *counter_out = cval;
+        return printf("hello world from % 6s! Count % d\n", name, cval);
     }
-    return gotsema;
+    return 0;
 }
